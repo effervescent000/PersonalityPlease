2f553c3940aa42e50d7d26c2e458427311e9acb8
effervescent000 <kislio01@gmail.com>
2024-02-13 14:53:55 -0500

    patch compatibility

---
+++ Source/Psychology Reborn/Harmony/Interactions/Pawn_RelationsTracker.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Personality.Harmony;
+
+[HarmonyPatch(typeof(Pawn_RelationsTracker), nameof(Pawn_RelationsTracker.CompatibilityWith))]
+public class Pawn_RelationsTracker_Patch
+{
+
+    private static AccessTools.FieldRef<object, Pawn> firstPawnRef = AccessTools.FieldRefAccess<Pawn>(typeof(Pawn_RelationsTracker), "pawn");
+
+    [HarmonyPrefix]
+    public static bool PatchCompatibility(Pawn otherPawn, ref float __result, Pawn_RelationsTracker __instance) {
+        Log.Message("Attempting to patch compatibility...");
+        Pawn pawnInst = new();
+
+        ref Pawn firstPawn = ref firstPawnRef.Invoke(__instance);
+
+        Log.Message($"Got {firstPawn} and {otherPawn}");
+        float baseCompatibility = CompatibilityHelper.EvaluateCompatibility(firstPawn, otherPawn);
+        Log.Message($"Compatibility is {baseCompatibility}");
+        __result = baseCompatibility;
+        Log.Message("Done!");
+        return true;
+    }
+
+}
--- Source/Psychology Reborn/Helpers/CompatibilityHelper.cs
+++ Source/Psychology Reborn/Helpers/CompatibilityHelper.cs
@@ -1,37 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Verse;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using Verse;
 
 namespace Personality
 {
     public static class CompatibilityHelper
     {
 
-        public static float EvaluateCompatibility(Pawn initiator, Pawn recipient) {
+        public static float EvaluateCompatibility(Pawn initiator, Pawn recipient)
+        {
             PsychologyComp initComp = PersonalityHelper.Comp(initiator);
             PsychologyComp reciComp = PersonalityHelper.Comp(recipient);
 
             List<PersonalityComparison> comparisons = new();
             List<PersonalityNodeDef> defs = PersonalityHelper.GetAll;
 
-            foreach (var def in defs)
+            foreach (PersonalityNodeDef def in defs)
             {
                 PersonalityComparison comparison = new(initValue: initComp.Psyche.nodes[def.defName].AdjustedRating, reciValue: reciComp.Psyche.nodes[def.defName].AdjustedRating, personalityDefName: def.defName);
                 comparisons.Add(comparison);
-                
+
             }
 
             float totalCompatibility = 1f;
-            foreach (var comparison in comparisons)
+            foreach (PersonalityComparison comparison in comparisons)
             {
                 float diff = comparison.Difference;
                 if (diff >= .25)
                 {
-                    totalCompatibility -= diff;
+                    totalCompatibility -= diff * ( 1f / defs.Count);
                 }
             }
             return Mathf.Clamp01(totalCompatibility);
