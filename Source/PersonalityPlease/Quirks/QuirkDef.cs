e12b3d312a1fcf2c4fcacac60256730b9b8337f9
effervescent000 <kislio01@gmail.com>
2024-02-28 08:38:58 -0500

    early quirk stuff

---
+++ Source/PersonalityPlease/Quirks/QuirkDef.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Personality;
+
+public class QuirkDef : Def
+{
+    public List<QuirkCategoryDef> categories;
+    public bool binary = false;
+}
